ed4f61aed0513a1c3359a1b5f39bbe37016e680e
박시은 <si990819@gmail.com>
2024-11-19 15:42:56 +0900

    nav형식 변경

--- src/ui/MainNav.tsx
+++ src/ui/MainNav.tsx
@@ -2,18 +2,24 @@ import styled from 'styled-components';
 
 const StyledMainNav = styled.nav`
   background-color: var(--color-grey-100);
-  /* border: 1px solid var(--color-grey-400); */
-  display: grid;
-  grid-template-columns: 4rem 4rem 4rem;
-  justify-content: center;
+
+  ul {
+    display: grid;
+    height: 100%;
+    background-color: aliceblue;
+    grid-template-columns: 4rem 4rem 4rem;
+    justify-content: center;
+  }
 `;
 
 const MainNav = () => {
   return (
     <StyledMainNav>
-      <div>nav1</div>
-      <div>nav2</div>
-      <div>nav3</div>
+      <ul>
+        <li>nav1</li>
+        <li>nav2</li>
+        <li>nav3</li>
+      </ul>
     </StyledMainNav>
   );
 };
